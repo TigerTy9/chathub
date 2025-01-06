13a61f0a6c27f33d73fa4168a6b6dce131a70be5
BotBridgeAI <botbridgeai@gmail.com>
2025-01-06 17:50:52 -0500

    s

--- docs/js/login.js
+++ docs/js/login.js
@@ -34,7 +34,7 @@ function loginUser() {
       //  window.opener.postMessage('loggedIn', window.location.origin);
 
          // Check the referrer
-        const redirectUrl = document.referrer || "https://botbridge.net/";
+        const redirectUrl = document.referrer || "https://dev.botbridge.net/";
         console.log('Redirecting to:', redirectUrl); // Log the redirect URL
         
         if (redirectUrl == "https://botbridge.net/chat.html") {
